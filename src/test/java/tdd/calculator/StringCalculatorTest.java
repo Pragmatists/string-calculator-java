fc614700a669c3901c47a1d1051d667bd1332fdc
michal.lipski <kiwaczki@gmail.com>
2021-06-23 11:47:59 +0200

    first 2 steps

--- src/test/java/tdd/calculator/StringCalculatorTest.java
+++ src/test/java/tdd/calculator/StringCalculatorTest.java
@@ -2,6 +2,7 @@ package tdd.calculator;
 
 import org.junit.jupiter.api.Test;
 
+import static java.util.Arrays.stream;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringCalculatorTest {
@@ -10,4 +11,23 @@ public class StringCalculatorTest {
     public void startHere() {
         assertThat(4).isEqualTo(5);
     }
+
+    @Test
+    public void AddOneNumberManyDigitsGivesNumber() {
+        assertThat(add("100")).isEqualTo(100);
+    }
+
+    @Test
+    public void AddManyNumbersGivesTheirSum() {
+        assertThat(add("10,20,30")).isEqualTo(60);
+    }
+
+    private Integer add(String numbers) {
+        if (numbers.isEmpty()) {
+            return 0;
+        }
+
+        return stream(numbers.split(",")).mapToInt(Integer::parseInt).sum();
+    }
+
 }
